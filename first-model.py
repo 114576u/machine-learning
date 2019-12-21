8620c18c01976e5eaa3e9e1a5fe68a0d19c22e04
114576u <114576u@gmail.com>
2019-12-21 21:39:51 +0100

    compile model, add label data, train the model and use the model

--- first-model.py
+++ first-model.py
@@ -9,3 +9,17 @@ model = tf.keras.Sequential(
     [keras.layers.Dense(units=1, input_shape=[1])]
 )
 
+model.compile(
+    optimizer='sgd',
+    loss='mean_squared_error'
+)
+
+# data
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+# train the neural network
+model.fit(xs, ys, epochs=500)
+
+# use the model
+print(model.predict([10.0]))
