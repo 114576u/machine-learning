056dc5d52215e571227a507c3ace4fe61a6adbd9
114576u <114576u@gmail.com>
2019-12-22 12:45:03 +0100

    comments added

--- first-model.py
+++ first-model.py
@@ -9,12 +9,15 @@ model = tf.keras.Sequential(
     [keras.layers.Dense(units=1, input_shape=[1])]
 )
 
+# loss function measures the guessed answers against the known correct answers and measures how well or how badly it did
+# optimizer is used to make another guess. Based on the loss function's result, it will try to minimize the loss
+#       sgd => stochastic gradient descent
 model.compile(
     optimizer='sgd',
     loss='mean_squared_error'
 )
 
-# data
+# provide the known data
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
